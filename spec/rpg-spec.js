1b5d585109aa20bce9c35d4e05485751974f9773
Matt Dowdney and William Chu <william@>
2018-04-18 11:08:39 -0700

    add jasmine tests for successful/failed battles and leveling up

--- spec/rpg-spec.js
+++ spec/rpg-spec.js
@@ -1,16 +1,43 @@
-import{ Character } from "./../src/rpg.js";
+import{ Character, battle } from "./../src/rpg.js";
 
 describe('Character', function() {
-  var reusableCharacter;
+  let reusableCharacter1;
+  let reusableCharacter2;
 
   beforeEach(function() {
-    reusableCharacter = new Character('boyband singer', 8, 3, 50);
+    reusableCharacter1 = new Character('boyband singer', 8, 3, 50);
+    reusableCharacter2 = new Character('thespian actor', 2, 2, 50);
   });
 
   it('should return Character object with reusableCharacter properties', function() {
-    expect(reusableCharacter.type).toEqual('boyband singer');
-    expect(reusableCharacter.hotness).toEqual(8);
-    expect(reusableCharacter.talent).toEqual(3);
-    expect(reusableCharacter.finances).toEqual(51);
+    expect(reusableCharacter1.type).toEqual('boyband singer');
+    expect(reusableCharacter1.hotness).toEqual(8);
+    expect(reusableCharacter1.talent).toEqual(3);
+    expect(reusableCharacter1.finances).toEqual(50);
+  });
+
+  it('should add Character.exp , Character.finances, and Character.esteem on successful battle', function(){
+    battle(reusableCharacter1);
+    expect(reusableCharacter1.finances).toEqual(100);
+    expect(reusableCharacter1.exp).toEqual(3);
+    expect(reusableCharacter1.esteem).toEqual(7);
+  });
+
+  it('should add Character.exp , subtract Character.finances, and subtract Character.esteem based on failed battle', function(){
+    battle(reusableCharacter2);
+    expect(reusableCharacter2.finances).toEqual(40);
+    expect(reusableCharacter2.exp).toEqual(1);
+    expect(reusableCharacter2.esteem).toEqual(4);
+  });
+
+  it('should show fame level 1 after Character.exp reaches 20', function(){
+    battle(reusableCharacter1);
+    battle(reusableCharacter1);
+    battle(reusableCharacter1);
+    battle(reusableCharacter1);
+    battle(reusableCharacter1);
+    battle(reusableCharacter1);
+    battle(reusableCharacter1);
+    expect(reusableCharacter1.fame).toEqual(1);
   });
 });
--- src/rpg.js
+++ src/rpg.js
@@ -5,19 +5,26 @@ export class Character {
     this.talent = talent;
     this.esteem = 5;
     this.finances = finances;
-    this.fame = 0;
     this.exp = 0;
+    this.fame = 0;
     this.inv = [];
+  }
+
+  levelCalc() {
+    if (this.exp >= 20) {
+      this.fame = 1;
+    }
   }
 }
 
 export function battle(charObj) {
-  if (charObj.talent >= 5) {
+  if (charObj.talent >= 5 || charObj.talent + (charObj.hotness / 2) >= 5) {
     charObj.finances += 50;
     charObj.exp += 3;
     charObj.esteem += 2;
   } else {
-    charObj.finance -= 10;
+    charObj.finances -= 10;
+    charObj.exp += 1;
     charObj.esteem -= 1;
   }
 }
