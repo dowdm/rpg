ef7db762d3f77b4c4d2d447e7bb201a4d618227f
Matt Dowdney and William Chu <william@>
2018-04-19 09:47:47 -0700

    refactor character.js class, update rpg-spec.js, tests successful

--- spec/rpg-spec.js
+++ spec/rpg-spec.js
@@ -1,7 +1,4 @@
-import { audition, performance, awards } from "./../src/battles.js";
 import { Character } from "./../src/character.js";
-import { buyMakeUp, buyHairSpray, buyMethodActing } from "./../src/purchaseinv.js";
-import { useMakeUp, useHairSpray, useMethodActing } from "./../src/consumeinv.js";
 
 describe('Character', function() {
   let reusableCharacter1;
@@ -26,57 +23,57 @@ describe('Character', function() {
   });
 
   it('should add Character.exp , Character.finances, and Character.esteem on successful audition', function(){
-    audition(reusableCharacter1);
+    reusableCharacter1.audition();
     expect(reusableCharacter1.finances).toEqual(100);
     expect(reusableCharacter1.exp).toEqual(3);
     expect(reusableCharacter1.esteem).toEqual(7);
   });
 
   it('should add Character.exp , subtract Character.finances, and subtract Character.esteem based on failed audition', function(){
-    audition(reusableCharacter2);
+    reusableCharacter2.audition();
     expect(reusableCharacter2.finances).toEqual(40);
     expect(reusableCharacter2.exp).toEqual(1);
     expect(reusableCharacter2.esteem).toEqual(4);
   });
 
   it('should show fame level 1 after Character.exp reaches 20', function(){
-    audition(reusableCharacter1);
-    audition(reusableCharacter1);
-    audition(reusableCharacter1);
-    audition(reusableCharacter1);
-    audition(reusableCharacter1);
-    audition(reusableCharacter1);
-    audition(reusableCharacter1);
+    reusableCharacter1.audition();
+    reusableCharacter1.audition();
+    reusableCharacter1.audition();
+    reusableCharacter1.audition();
+    reusableCharacter1.audition();
+    reusableCharacter1.audition();
+    reusableCharacter1.audition();
     expect(reusableCharacter1.fame).toEqual(1);
   });
 
   it('should add Character.exp , Character.finances, and Character.esteem on successful performance', function(){
-    performance(reusableCharacter3);
+    reusableCharacter3.performance();
     expect(reusableCharacter3.finances).toEqual(250);
     expect(reusableCharacter3.exp).toEqual(10);
     expect(reusableCharacter3.esteem).toEqual(15);
   });
 
   it('should buy makeup, add to Character.inv and subtract Character.finances', function(){
-    buyMakeUp(reusableCharacter3);
+    reusableCharacter3.buyMakeUp();
     expect(reusableCharacter3.finances).toEqual(130);
     expect(reusableCharacter3.inv).toContain('makeup');
   });
 
   it('should consume makeup, add 2 to Character.hotness and remove makeup from Character.inv', function(){
-    buyMakeUp(reusableCharacter3);
-    buyMakeUp(reusableCharacter3);
+    reusableCharacter3.buyMakeUp();
+    reusableCharacter3.buyMakeUp();
     expect(reusableCharacter3.inv).toContain('makeup','makeup');
-    useMakeUp(reusableCharacter3);
+    reusableCharacter3.useMakeUp();
     expect(reusableCharacter3.hotness).toEqual(3);
     expect(reusableCharacter3.inv).toContain('makeup');
   });
 
   it('should consume hairspray, add 3 to Character.hotness, add 2 to Character.esteem and remove hairspray from Character.inv', function(){
-    buyHairSpray(reusableCharacter3);
-    buyHairSpray(reusableCharacter3);
+    reusableCharacter3.buyHairSpray();
+    reusableCharacter3.buyHairSpray();
     expect(reusableCharacter3.inv).toContain('hairspray','hairspray');
-    useHairSpray(reusableCharacter3);
+    reusableCharacter3.useHairSpray();
     expect(reusableCharacter3.hotness).toEqual(4);
     expect(reusableCharacter3.esteem).toEqual(7);
     expect(reusableCharacter3.inv).toContain('hairspray');
@@ -84,14 +81,14 @@ describe('Character', function() {
 
   it('should increase charObj.finances by 650 on successful performance for Character with charObj.fame 5', function(){
     reusableCharacter4.levelCalc();
-    performance(reusableCharacter4);
+    reusableCharacter4.performance();
     expect(reusableCharacter4.finances).toEqual(650);
   });
 
   it('should consume methodacting, increase charObj.talent by 3, increase charObj.esteem by 1 and remove methodacting from Character.inv', function(){
-    buyMethodActing(reusableCharacter3);
+    reusableCharacter3.buyMethodActing();
     expect(reusableCharacter3.inv).toContain('methodacting');
-    useMethodActing(reusableCharacter3);
+    reusableCharacter3.useMethodActing();
     expect(reusableCharacter3.talent).toEqual(12);
     expect(reusableCharacter3.esteem).toEqual(6);
   });
--- src/character.js
+++ src/character.js
@@ -9,7 +9,8 @@ class Character {
     this.fame = 0;
     this.inv = [];
   }
-
+  
+  //LEVEL METER
   levelCalc() {
     if (this.exp >= 100) {
       this.fame = 5;
@@ -23,6 +24,88 @@ class Character {
       this.fame = 1;
     } else if (this.exp >= 0 && this.exp < 20) {
       this.fame = 0;
+    }
+  }
+
+  //BATTLE TYPES
+  audition() {
+    if (this.talent >= 5 || this.talent + (this.hotness / 2) >= 5) {
+      this.finances += 50 * (1 + this.fame);
+      this.exp += 3;
+      this.esteem += 2;
+    } else {
+      this.finances -= 10;
+      this.exp += 1;
+      this.esteem -= 1;
+    }
+    this.levelCalc();
+  }
+  performance() {
+    if (this.talent >= 8 || this.talent + (this.hotness / 3) >= 8) {
+      debugger;
+      this.finances += 100 * (1 + this.fame);
+      this.exp += 10;
+      this.esteem += 10;
+    } else {
+      this.finances -= 100;
+      this.exp -= 1;
+      this.esteem -= 10;
+    }
+    this.levelCalc();
+  }
+  awards() {
+    if (this.hotness >= 8 || this.hotness + (this.talent / 2) >= 5) {
+      this.finances += 300  * (1 + this.fame);
+      this.exp += 30;
+      this.esteem += 20;
+    } else {
+      this.finances -= 100;
+      this.exp += 3;
+    }
+    this.levelCalc();
+  }
+  //PURCHASE INVENTORY
+  buyMakeUp() {
+    if (this.finances >= 20) {
+    this.inv.push('makeup');
+    this.finances -= 20;
+    }
+  }
+  buyHairSpray() {
+    if (this.finances >= 40) {
+    this.inv.push('hairspray');
+    this.finances -= 40;
+    }
+  }
+  buyMethodActing() {
+    if (this.finances >= 150) {
+    this.inv.push('methodacting');
+    this.finances -= 150;
+    }
+  }
+
+  //USE INVENTORY
+  useMakeUp() {
+    if (this.inv.includes('makeup')) {
+      this.hotness += 2;
+      let removeItemIndex = this.inv.indexOf('makeup');
+      this.inv.splice(removeItemIndex, 1);
+    }
+  }
+  useHairSpray() {
+    if (this.inv.includes('hairspray')) {
+      this.hotness += 3;
+      this.esteem += 2;
+      let removeItemIndex = this.inv.indexOf('hairspray');
+      this.inv.splice(removeItemIndex, 1);
+    }
+  }
+  useMethodActing() {
+    if (this.inv.includes('methodacting')) {
+      this.talent += 3;
+      this.esteem += 1;
+      let removeItemIndex = this.inv.indexOf('methodacting');
+      this.inv.splice(removeItemIndex, 1);
     }
   }
 }
